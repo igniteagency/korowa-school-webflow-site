5ede05415fd24d66eff876070a709372070b7d07
Paras Shah <parasshah195@gmail.com>
2025-08-15 19:38:13 +0530

    Initial commit

---
+++ src/global.ts
@@ -0,0 +1,30 @@
+import { animatedDetailsAccordions } from '$components/accordions';
+import '$components/dialog';
+import { setCurrentYear } from '$utils/current-year';
+import '$utils/disable-webflow-scroll';
+import { disableWebflowAnchorSmoothScroll } from '$utils/disable-webflow-scroll';
+import handleExternalLinks from '$utils/external-link';
+import addMainElementId from '$utils/main-element-id';
+import { duplicateMarqueeList } from '$utils/marquee-list';
+
+gsap.registerPlugin(ScrollTrigger);
+
+window.Webflow = window.Webflow || [];
+window.Webflow?.push(() => {
+  // Set current year on respective elements
+  setCurrentYear();
+  addMainElementId();
+  handleExternalLinks();
+
+  UIFunctions();
+  webflowOverrides();
+});
+
+function UIFunctions() {
+  duplicateMarqueeList();
+  animatedDetailsAccordions();
+}
+
+function webflowOverrides() {
+  disableWebflowAnchorSmoothScroll();
+}
