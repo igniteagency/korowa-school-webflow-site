a9def49dba1f9ce43089259527cd219762bd21b9
Paras Shah <parasshah195@gmail.com>
2025-09-15 12:02:40 +0530

    Add nav scroll functionality

--- src/global.ts
+++ src/global.ts
@@ -1,6 +1,7 @@
 import { animatedDetailsAccordions } from '$components/accordions';
 import Dialog from '$components/dialog';
 import NavMenu from '$components/nav/menu';
+import { navbarScrollToggle } from '$components/nav/scroll';
 import { setCurrentYear } from '$utils/current-year';
 import '$utils/disable-webflow-scroll';
 import { disableWebflowAnchorSmoothScroll } from '$utils/disable-webflow-scroll';
