dc128f54f6dd4c1e398363f6a4de357e61fa7e03
Paras Shah <parasshah195@gmail.com>
2026-02-11 00:39:36 +0530

    Update IX3 init logic
    retries in case initial try didn't work

--- src/global.ts
+++ src/global.ts
@@ -14,10 +14,35 @@ import { addSafariBrowserClass } from '$utils/safari-detection';
 
 window.Webflow = window.Webflow || [];
 window.Webflow?.push(() => {
-  setTimeout(() => {
-    window.WF_IX = Webflow.require('ix3');
-    console.debug('Webflow IX3 globalised:', window.WF_IX);
-  }, 100);
+  const WF_IX3_CHECK_MAX_TRIES = 5;
+  let wfIx3CheckTries = 0;
+
+  function setWFIX3Global() {
+    if (!window.WF_IX && wfIx3CheckTries < WF_IX3_CHECK_MAX_TRIES) {
+      setTimeout(() => {
+        console.debug(
+          'Checking for Webflow IX3 module...',
+          `Attempt ${wfIx3CheckTries}`,
+          window.Webflow.require('ix3')
+        );
+
+        window.WF_IX = window.Webflow.require('ix3');
+        if (window.WF_IX) {
+          console.debug('Webflow IX3 globalised:', window.WF_IX);
+          return;
+        }
+
+        setWFIX3Global();
+      }, 500);
+      wfIx3CheckTries++;
+    } else {
+      console.error(
+        'Webflow IX3 failed to globalise after multiple attempts. Some interactions may not work as expected.'
+      );
+    }
+  }
+
+  setWFIX3Global();
 
   // Set current year on respective elements
   setCurrentYear();
